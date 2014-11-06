fe2f68880d9953b090fa693e2a417681d4ea66f2
akshaymuley <akshaymuley@gmail.com>
2014-11-07 00:16:26 +0530

    Added test project

---
+++ ChennaiExpressTest/FareCalculatorTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Chennai_Express;
+
+namespace ChennaiExpressTest
+{
+    [TestClass]
+    public class FareCalculatorTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Ticket ticket = new Ticket("Station 1", "Station 11", 24, 20);
+            ITrain chennaiExpress = new ChennaiExpress();
+            IFareCalculator fareCalculator = new FareCalculator(chennaiExpress);
+            
+            Assert.AreEqual(15, fareCalculator.GetFare(ticket));
+        }
+    }
+}
